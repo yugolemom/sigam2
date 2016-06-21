0ef85dec17fcde5054513e771e3f7e816b5c7357
1553910 <yugolemom@gmail.com>
2016-06-21 15:37:10 -0300

    Iniciando Criação do Formulario Inicial

---
+++ app/src/main/java/br/unitins/sigam2/controllers/FormActivity.java
@@ -0,0 +1,15 @@
+package br.unitins.sigam2.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.unitins.sigam2.R;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
